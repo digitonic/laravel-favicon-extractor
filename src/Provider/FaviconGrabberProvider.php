0578b795ad47dac66d65711c15a29f55a57cec9b
Christopher Crawford <christopher@digitonic.co.uk>
2020-01-20 12:20:46 +0000

    Find the highest quality favicon if multiple exist on the page

--- src/Provider/FaviconGrabberProvider.php
+++ src/Provider/FaviconGrabberProvider.php
@@ -3,6 +3,7 @@
 namespace StefanBauer\LaravelFaviconExtractor\Provider;
 
 use GrahamCampbell\GuzzleFactory\GuzzleFactory;
+use Illuminate\Support\Collection;
 use StefanBauer\LaravelFaviconExtractor\Exception\NoIconsFoundException;
 
 class FaviconGrabberProvider implements ProviderInterface
@@ -13,12 +14,44 @@ class FaviconGrabberProvider implements ProviderInterface
         $client = GuzzleFactory::make();
         $response = $client->get($this->getUrl($url['path']));
         $icons = json_decode($response->getBody()->getContents(), true)['icons'];
+        $collectedIcons = collect($icons);
 
         if ($icons === null) {
             throw new NoIconsFoundException('No icons could be found for this url', '404');
         }
 
-        return $icons[0]['src'];
+        $filteredIcons = $collectedIcons->filter(function ($value, $key) {
+            if (! empty($value['sizes'])) {
+                return $value;
+            }
+        });
+
+        if ($filteredIcons->isEmpty()) {
+            return $icons[0]['src'];
+        }
+
+        return $this->findHighestQualityIcon($filteredIcons);
+    }
+
+    /**
+     * @param Collection $icons
+     *
+     * @return string
+     */
+    private function findHighestQualityIcon(Collection $icons): string
+    {
+        $highestQualitySource = '';
+        $largestSize = 0;
+
+        $icons->each(function ($processedIcon) use (&$highestQualitySource, &$largestSize) {
+            $iconSize = explode('x', $processedIcon['sizes'])[0];
+            if ($iconSize > $largestSize ) {
+                $highestQualitySource = $processedIcon['src'];
+                return;
+            }
+        });
+
+        return $highestQualitySource;
     }
 
     private function getUrl(string $url): string
